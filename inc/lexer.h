96cf96224027c46c3ffaca26b78c34bb42cbb94c
nicolasgsantana <nicolas07san@gmail.com>
2026-01-12 14:57:00 -0300

    feat: add lexer header

---
+++ inc/lexer.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/12 10:47:26 by nde-sant          #+#    #+#             */
+/*   Updated: 2026/01/12 14:56:26 by nde-sant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+
+#include <stddef.h>
+
+typedef enum e_token_type
+{
+	TOKEN_EOF = 0,
+}	t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	const char		*text;
+	const size_t	text_size;
+}					t_token;
+
+#endif
