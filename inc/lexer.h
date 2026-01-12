190604e3492c3580503ba7cc82373d6eada58d52
nicolasgsantana <nicolas07san@gmail.com>
2026-01-12 19:24:42 -0300

    feat: add token types

--- inc/lexer.h
+++ inc/lexer.h
@@ -6,18 +6,20 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 10:47:26 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/01/12 14:56:26 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/01/12 19:23:31 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_H
 # define LEXER_H
 
-#include <stddef.h>
+# include <stddef.h>
 
 typedef enum e_token_type
 {
 	TOKEN_EOF = 0,
+	TOKEN_WORD,
+	TOKEN_OPERATOR,
 }	t_token_type;
 
 typedef struct s_token
