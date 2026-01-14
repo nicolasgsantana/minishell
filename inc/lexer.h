6eadb1a2a4c91aed92b092b8fab53312ae203edd
nicolasgsantana <nicolas07san@gmail.com>
2026-01-14 15:06:19 -0300

    refactor: add 'TK_' prefix to token types

--- inc/lexer.h
+++ inc/lexer.h
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 10:47:26 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/01/14 09:55:32 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/01/14 10:47:41 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@
 
 typedef enum e_token_type
 {
-	EOF = 0,
-	WORD,
-	OPERATOR,
-	REDIR_OP,
-	VAR,
-	S_QUOTE,
-	D_QUOTE,
+	TK_EOF = 0,
+	TK_WORD,
+	TK_OPERATOR,
+	TK_REDIR_OP,
+	TK_VAR,
+	TK_S_QUOTE,
+	TK_D_QUOTE,
 }	t_token_type;
 
 typedef struct s_token
 {
 	t_token_type	type;
 	const char		*text;
-	const size_t	text_size;
+	size_t			text_size;
 }					t_token;
 
 #endif
