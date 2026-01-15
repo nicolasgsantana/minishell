846b6aa514ea163ea6a8856d95febfdadfdcafef
kqueiroz <keilaq.cortes@gmail.com>
2026-01-15 13:48:33 -0300

    feat(builtins): implement echo builtin

---
+++ src/builtins/ft_echo.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keila <keila@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/14 19:18:45 by kqueiroz          #+#    #+#             */
+/*   Updated: 2026/01/15 14:06:35 by keila            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_echo(t_cmd *cmd)
+{
+	int	i;
+	int	newline;
+
+	i = 1;
+	newline = 1;
+	if (cmd->argv[1] && !ft_strcmp(cmd->argv[1], "-n"))
+	{
+		newline = 0;
+		i++;
+	}
+	while (cmd->argv[i])
+	{
+		ft_putstr_fd(cmd->argv[i], STDOUT_FILENO);
+		if (cmd->argv[i + 1])
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		i++;
+	}
+	if (newline)
+		ft_putchar_fd('\n', STDOUT_FILENO);
+	return (0);
+}
