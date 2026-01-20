1d2e0246c49ad93b6869aed43d1d341557e0a48d
kqueiroz <keilaq.cortes@gmail.com>
2026-01-20 17:45:33 -0300

    feat(builtins): implement export and unset

---
+++ src/builtins/builtins_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keila <keila@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/16 10:30:06 by kqueiroz          #+#    #+#             */
+/*   Updated: 2026/01/17 13:13:14 by keila            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_valid_identifier(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!ft_isalpha(str[0]) && str[0] != '_')
+		return (0);
+	while (str[i] && str[i] != '=')
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	env_len(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	return (i);
+}
+
+int	find_env_index(char **envp, char *key)
+{
+	int		i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(key);
+	while (envp[i])
+	{
+		if (!ft_strncmp(envp[i], key, len) && envp[i][len] == '=')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+void	print_env(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		printf("%s\n", envp[i]);
+		i++;
+	}
+}
