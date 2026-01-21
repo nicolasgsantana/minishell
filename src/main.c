b5eac3e377bf99aa86cacb76827903c8e42f5848
nicolasgsantana <nicolas07san@gmail.com>
2026-01-21 16:42:29 -0300

    feat: add print tokens for debug purposes

--- src/main.c
+++ src/main.c
@@ -6,13 +6,14 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 10:59:11 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/01/21 16:19:56 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/01/21 16:41:00 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 char	*token_name(t_tk_type type);
+void	print_tokens(t_list *tokens);
 
 int	main(void)
 {
@@ -29,6 +30,7 @@ int	main(void)
 		{
 			add_history(line);
 			tokens = tokenize(line);
+			print_tokens(tokens);
 			free_token_lst(tokens);
 		}
 		free(line);
@@ -59,5 +61,17 @@ char	*token_name(t_tk_type type)
 			return "heredoc";
 		default:
 			return NULL;
+	}
+}
+
+void	print_tokens(t_list *tokens)
+{
+	t_token	*token;
+
+	while (tokens)
+	{
+		token = tokens->content;
+		ft_printf("%s (%s)\n", token->text, token_name(token->type));
+		tokens = tokens->next;
 	}
 }
