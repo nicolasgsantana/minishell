e00b4510e76685992dc088eabf53e0eb99611183
nicolasgsantana <nicolas07san@gmail.com>
2026-01-27 14:30:29 -0300

    feat: add check_quotes function

--- inc/parser.h
+++ inc/parser.h
@@ -6,13 +6,17 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 10:06:56 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/01/26 10:15:21 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/01/27 14:29:51 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-# include "libft.h"
+# include "minishell.h"
+
+# define QT_ERR "Error: Open quotes\n"
+
+int		check_quotes(t_list	*tokens);
 
 #endif
