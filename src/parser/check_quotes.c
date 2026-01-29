a6f48d57369e5172ab90e7def1fd53aadf51fada
nicolasgsantana <nicolas07san@gmail.com>
2026-01-29 14:23:00 -0300

    refactor: update to norm

--- src/parser/check_quotes.c
+++ src/parser/check_quotes.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 13:52:21 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/01/27 14:24:22 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/01/29 09:46:16 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ static int	quote_count(t_token *token)
 	while (token->text[i])
 	{
 		if (token->expansion == EXP_DQUOTED)
-			if(token->text[i] == '"')
+			if (token->text[i] == '"')
 				count++;
-		if(token->expansion == EXP_SQUOTED)
+		if (token->expansion == EXP_SQUOTED)
 			if (token->text[i] == '\'')
 				count++;
 		i++;
@@ -40,11 +40,13 @@ int	check_quotes(t_list	*tokens)
 	{
 		token = tokens->content;
 		if (token->expansion != EXP_NONE)
+		{
 			if (quote_count(token) != 2)
 			{
 				ft_putstr_fd(QT_ERR, STDERR_FILENO);
 				return (1);
 			}
+		}
 		tokens = tokens->next;
 	}
 	return (0);
