bfc33edeebc4d0b8801226bc4d14c288067c4a76
nicolasgsantana <nicolas07san@gmail.com>
2026-02-09 11:31:12 -0300

    fix: var name comparison now use biggest length between env and input strings

--- src/expansion/get_var.c
+++ src/expansion/get_var.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 15:51:08 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/02/05 15:50:35 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/02/09 11:17:15 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,74 +17,34 @@ static char	*get_var_name(char *text)
 	int	i;
 
 	i = 0;
+	if (text[i] == '$')
+		i++;
 	while (text[i])
 	{
-		if (ft_isspace(text[i]) || text[i] == '"')
+		if (!valid_name_char(text[i]))
 			break ;
 		i++;
 	}
 	return (ft_substr(text, 1, i - 1));
 }
 
-static size_t	envp_len(char **envp)
-{
-	size_t	len;
-
-	len = 0;
-	while (envp[len])
-		len++;
-	return (len);
-}
-
-static char	*get_envp_name(char *line)
-{
-	char	**split;
-	char	*name;
-	int		i;
-
-	split = ft_split(line, '=');
-	name = ft_strdup(name);
-	i = 0;
-	while (split[i])
-		free(split[i++]);
-	return (name);
-}
-
-static char	**var_name_list(char **envp)
-{
-	char	**var_names;
-	int		i;
-
-	var_names = malloc(sizeof(char *) * (envp_len(envp) + 1));
-	if (!var_names);
-		return (NULL);
-	i = 0;
-	while (envp[i])
-	{
-		var_names[i] = get_envp_name(envp[i]);
-		i++;
-	}
-	return (var_names);
-}
-
 char	*get_var(char *text, char **envp)
 {
 	char	*var_name;
-	char	**var_list;
-	int	i;
-	int	j;
+	int		i;
+	size_t	j;
 
 	i = 0;
 	var_name = get_var_name(text);
-	var_list = var_name_list(envp);
 	while(envp[i])
 	{
-		if (!ft_strncmp(var_name, envp[i], ft_strlen(var_name)))
-		{
-			j = 0;
-			while(envp[i][j] != '=')
-				j++;
+		j = 0;
+		while(envp[i][j] != '=')
 			j++;
+		if (j < ft_strlen(var_name))
+			j = ft_strlen(var_name);
+		if (!ft_strncmp(var_name, envp[i], j++))
+		{
 			free(var_name);
 			return (ft_substr(envp[i], j, ft_strlen(envp[i]) - j));
 		}
