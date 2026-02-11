186b99dbb4d36ecaace10aff52dfb9e35e4bcca7
nicolasgsantana <nicolas07san@gmail.com>
2026-02-11 18:40:47 -0300

    refactor(parser): remove unnused debug functions

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,14 +6,11 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 10:14:53 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/02/11 18:37:13 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/02/11 18:39:50 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-char	*token_name(t_tk_type type);
-void	print_tokens(t_list *tokens);
 
 t_cmd	*new_cmd(void)
 {
@@ -156,40 +153,4 @@ int	parse(char *line, t_shell *sh)
 	}
 	append_cmd(sh, cmd);
 	return (0);
-}
-
-// !!!DEBUG ONLY REMOVE LATER
-char	*token_name(t_tk_type type)
-{
-	switch (type)
-	{
-		case TK_EOF:
-			return "end of file";
-		case TK_WORD:
-			return "word";
-		case TK_PIPE:
-			return "pipe";
-		case TK_RD_OUT:
-			return "rd out";
-		case TK_RD_IN:
-			return "rd in";
-		case TK_AP_OUT:
-			return "ap out";
-		case TK_HEREDOC:
-			return "heredoc";
-		default:
-			return NULL;
-	}
-}
-
-void	print_tokens(t_list *tokens)
-{
-	t_token	*token;
-
-	while (tokens)
-	{
-		token = tokens->content;
-		ft_printf("%s (%s)\n", token->text, token_name(token->type));
-		tokens = tokens->next;
-	}
 }
